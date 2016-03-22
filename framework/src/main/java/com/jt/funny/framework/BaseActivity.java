486a5157a5571ed37763b84bbb3181244df4359d
斗破 <goddard.jt@alibaba-inc.com>
2016-03-22 22:48:19 +0800

    add BaseActivity

---
+++ framework/src/main/java/com/jt/funny/framework/BaseActivity.java
@@ -0,0 +1,39 @@
+package com.jt.funny.framework;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by jiangtao on 16/3/22.
+ *
+ * @author goddard.jt
+ * @version 1.0.0
+ */
+public class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
